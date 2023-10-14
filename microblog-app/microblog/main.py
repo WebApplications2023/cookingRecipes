81098359f06ce9851cf0d6d80c05e5c982c39491
SophiaWeiler <sophia.weiler@outlook.com>
2023-10-14 18:35:25 +0200

    sw - adding information from lab3

--- microblog-app/microblog/main.py
+++ microblog-app/microblog/main.py
@@ -2,6 +2,7 @@ import datetime
 import dateutil.tz
 
 from flask import Blueprint, render_template
+import flask_login
 
 
 from . import model
@@ -10,62 +11,81 @@ bp = Blueprint("main", __name__)
 
 
 @bp.route("/")
+@flask_login.login_required
 def index():
-    user = model.User(1, "sophia@example.com", "Sophia Weiler", "sophiaweiler", "sophia.png")
-    user2 = model.User(2, "maeve@example.com", "Maeve Balavender", "maeveb", "maeve.png")
-    user3 = model.User(3, "kate@example.com", "Kate Roth", "kroth", "kate.png")
+
+    img_sophia = open('./microblog/static/resources/sophia.png', 'rb').read()
+    img_maeve = open('./microblog/static/resources/maeve.png', 'rb').read()
+    img_kate = open('./microblog/static/resources/kate.png', 'rb').read()
+    
+    user = model.User(email = "sophia@example.com", name = "Sophia Weiler", handle = "sophiaweiler", img = img_sophia, password = "123")
+    user2 = model.User(email = "maeve@example.com", name = "Maeve Balavender", handle = "maeveb", img = img_maeve, password = "123")
+    user3 = model.User(email = "kate@example.com", name = "Kate Roth", handle = "kroth", img = img_kate, password = "123")
 
     posts = [
         model.Message(
-            1, user2, "So happy to be done with school for today.", datetime.datetime.now(dateutil.tz.tzlocal())
+            user = user2, 
+            text = "So happy to be done with school for today.", 
+            timestamp = datetime.datetime.now(dateutil.tz.tzlocal())
         ),
         model.Message(
-            2, user, "Just working on my Web Applications homework!!!", datetime.datetime.now(dateutil.tz.tzlocal())
-        ),
-         model.Message(
-            3, user3, "Finally made it back to Cork :)", datetime.datetime.now(dateutil.tz.tzlocal())
-        ),
-        model.Message(
-            4, user, "Oktoberfest is so much fun!", datetime.datetime.now(dateutil.tz.tzlocal())
+            user = user, 
+            text = "Just working on my Web Applications homework!!!",
+            timestamp = datetime.datetime.now(dateutil.tz.tzlocal())
         ),
         model.Message(
-            4, user2, "What should I pack for this weekend?", datetime.datetime.now(dateutil.tz.tzlocal())
+            user = user3, 
+            text = "Finally made it back to Cork :)",
+            timestamp = datetime.datetime.now(dateutil.tz.tzlocal())
         ),
         model.Message(
-            4, user3, "Getting SOOOO excited to see everyone!!!!", datetime.datetime.now(dateutil.tz.tzlocal())
+            user = user, 
+            text = "Oktoberfest is so much fun!", 
+            timestamp = datetime.datetime.now(dateutil.tz.tzlocal())
+        ),
+        model.Message(
+            user = user2, 
+            text = "What should I pack for this weekend?", 
+            timestamp = datetime.datetime.now(dateutil.tz.tzlocal())
+        ),
+        model.Message(
+            user = user3, 
+            text = "Getting SOOOO excited to see everyone!!!!",
+            timestamp = datetime.datetime.now(dateutil.tz.tzlocal())
         ),
     ]
+    
     return render_template("main/index.html", posts=posts, user=user)
 
-@bp.route("/profile")
-def profile():
-    user = model.User(2, "soph@example.com", "Sophia Weiler", "sophiaweiler", "sophia.png")
-    posts = [
-        model.Message(
-            2, user, "Just working on my Web Applications homework!!!", datetime.datetime.now(dateutil.tz.tzlocal())
-        ),
-        model.Message(
-            4, user, "Oktoberfest is so much fun!", datetime.datetime.now(dateutil.tz.tzlocal())
-        ),
-    ]
-    return render_template("main/profile.html", posts=posts, user=user)
 
+@bp.route("/profile")
+@flask_login.login_required
+def profile():
+    return render_template("main/profile.html")
+
+"""
 @bp.route("/postResponse")
+@flask_login.login_required
 def postResponse():
-    user = model.User(2, "soph@example.com", "Sophia Weiler", "sophiaweiler", "sophia.png")
-    user2 = model.User(2, "maeve@example.com", "Maeve Balavender", "maeveb", "maeve.png")
-    user3 = model.User(3, "kate@example.com", "Kate Roth", "kroth", "kate.png")
+    user = model.User("soph@example.com", "Sophia Weiler", "sophiaweiler", "sophia.png")
+    user2 = model.User("maeve@example.com", "Maeve Balavender", "maeveb", "maeve.png")
+    user3 = model.User("kate@example.com", "Kate Roth", "kroth", "kate.png")
     posts = [
         model.Message(
-            2, user, "Just working on my Web Applications homework!!!", datetime.datetime.now(dateutil.tz.tzlocal())
+            user = user, 
+            text = "Just working on my Web Applications homework!!!", 
+            timestamp = datetime.datetime.now(dateutil.tz.tzlocal())
         ),
     ]
     responses = [
         model.Message(
-            2, user3, "CS Queen slay", datetime.datetime.now(dateutil.tz.tzlocal())
+            user = user3, 
+            text = "CS Queen slay", 
+            timestamp = datetime.datetime.now(dateutil.tz.tzlocal())
         ),
         model.Message(
             2, user2, "Let's go to the cafe soon!", datetime.datetime.now(dateutil.tz.tzlocal())
         ),
     ]
     return render_template("main/postResponse.html", posts=posts, user=user, responses=responses)
+"""
--- microblog-app/microblog/model.py
+++ microblog-app/microblog/model.py
@@ -1,4 +1,4 @@
-class Message:
+"""class Message:
     def __init__(self, message_id, user, text, timestamp):
         self.message_id = message_id
         self.user = user
@@ -13,3 +13,26 @@ class User:
         self.name = name
         self.handle = handle
         self.img = img
+"""
+
+from . import db
+import flask_login
+
+class User(flask_login.UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    name = db.Column(db.String(64), nullable=False)
+    handle = db.Column(db.String(64), nullable=False)
+    img = db.Column(db.LargeBinary, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    messages = db.relationship('Message', back_populates='user')
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', back_populates='messages')
+    text = db.Column(db.String(512), nullable=False)
+    timestamp = db.Column(db.DateTime(), nullable=False)
+    response_to_id = db.Column(db.Integer, db.ForeignKey('message.id'))
+    response_to = db.relationship('Message', back_populates='responses', remote_side=[id])
+    responses = db.relationship('Message', back_populates='response_to', remote_side=[response_to_id])
