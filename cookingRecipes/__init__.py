1e399e3b1fb62558ed3d7e230313089a3aa35a9a
SophiaWeiler <sophia.weiler@outlook.com>
2023-11-08 11:50:41 +0100

    sw - adding basis for user and recipe db, adding _init_ file and auth file for login

---
+++ cookingRecipes/__init__.py
@@ -0,0 +1,44 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+import base64
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+
+
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+
+
+    # A secret for signing session cookies
+    app.config["SECRET_KEY"] = ""
+    
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///cookingRecipes.db"
+    # Register blueprints
+    # (we import main from here to avoid circular imports in the next lab)
+    from . import main
+    from . import auth
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+    from . import model
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return db.session.get(model.User, int(user_id))
+    
+    def base64_encode(blob_data):
+        return base64.b64encode(blob_data).decode('utf-8')
+    app.jinja_env.filters['base64_encode'] = base64_encode
+
+    app.register_blueprint(main.bp)
+    app.register_blueprint(auth.bp)
+
+    db.init_app(app)
+    return app
+
+
---
+++ cookingRecipes/model.py
@@ -0,0 +1,60 @@
+from . import db
+import flask_login
+
+
+class User(flask_login.UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    name = db.Column(db.String(64), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    recipes = db.relationship('Recipes', back_populates='user')
+    """messages = db.relationship('Message', back_populates='user')
+    following = db.relationship(
+        "User",
+        secondary=FollowingAssociation.__table__,
+        primaryjoin=FollowingAssociation.follower_id == id,
+        secondaryjoin=FollowingAssociation.followed_id == id,
+        back_populates="followers",
+    )
+    followers = db.relationship(
+        "User",
+        secondary=FollowingAssociation.__table__,
+        primaryjoin=FollowingAssociation.followed_id == id,
+        secondaryjoin=FollowingAssociation.follower_id == id,
+        back_populates="following",
+    )
+
+    
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user = db.relationship('User', back_populates='messages')
+    text = db.Column(db.String(512), nullable=False)
+    timestamp = db.Column(db.DateTime(), nullable=False)
+    response_to_id = db.Column(db.Integer, db.ForeignKey('message.id'))
+    response_to = db.relationship('Message', back_populates='responses', remote_side=[id])
+    responses = db.relationship('Message', back_populates='response_to', remote_side=[response_to_id])
+
+    
+
+class FollowingAssociation(db.Model):
+    follower_id = db.Column(
+        db.Integer, db.ForeignKey("user.id"), primary_key=True, nullable=False
+    )
+    followed_id = db.Column(
+        db.Integer, db.ForeignKey("user.id"), primary_key=True, nullable=False
+    )
+      """
+    
+class Recipe(flask_login.UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(64), nullable=False)
+    description = db.Column(db.String(512), nullable = False)
+    user = db.relationship('User', back_populates='recipes')
+    numPeople = db.Column(db.Integer, nullable=False)
+    cookingTime = db.Column(db.Integer, nullable=False) #number of minutes
+    img = db.Column(db.LargeBinary, nullable=False)
+
+    
