88826dc11855aa8e95613ec594ae3e3972113c73
SophiaWeiler <sophia.weiler@outlook.com>
2023-11-15 18:14:19 +0100

    sw - form set up for testing

--- cookingRecipes/static/cooking.js
+++ cookingRecipes/static/cooking.js
@@ -36,9 +36,9 @@ var addIngredient = function(){
     val.append('<option value="other">Other</option>')
 
     var button = $("<button>")
-        .addClass("removeIngredient")
+        .addClass("remove")
         .text("Remove")
-        .click(remove);
+        .click(remove(".ingredientList"));
         
     $(div).append(quant);
     $(div).append(val);
@@ -50,10 +50,14 @@ var addIngredient = function(){
         if ($(this).val() === 'other') {
             var newInput = $("<input>")
                 .addClass("ingredientQuant")
+                .addClass("otherQuant")
                 .attr("type", "text")
                 .attr("placeholder", "Specify Other Quantity");
             
             $(div).append(newInput);
+        }
+        else{
+            $(".otherQuant").remove();
         }
     });
 
@@ -61,12 +65,36 @@ var addIngredient = function(){
         if ($(this).val() === 'other') {
             var newInput = $("<input>")
                 .addClass("ingredientItem")
+                .addClass("otherIngredient")
                 .attr("type", "text")
                 .attr("placeholder", "Specify Other Ingredient");
             
             $(div).append(newInput);
         }
+        else{
+            $(".otherIngredient").remove();
+        }
     });
+}
+
+
+var addStep = function(){
+    var div = $("<div>")
+        .addClass("stepList");
+    var newStep = $("<input>")
+        .addClass("step")
+        .attr("type", "text")
+        .attr("placeholder", "Enter new step");
+
+    var button = $("<button>")
+        .addClass("remove")
+        .text("Remove")
+        .click(remove(".stepList"));
+        
+    $(div).append(newStep);
+    $(div).append(button);
+    
+    $(".addStep").before(div);
 }
 
 var getList = function(type){
@@ -80,20 +108,38 @@ var getList = function(type){
     return returnVal;
 }
 
-var remove = function(){
-    $(document).on("click", ".removeIngredient", function() {
-        $(this).closest(".ingredientList").remove();
+var remove = function(value){
+    $(document).on("click", ".remove", function() {
+        $(this).closest(value).remove();
     });
 }
 
 $(document).ready(function() {
     $(".addIngredient").click(addIngredient)
+    $(".addStep").click(addStep);
     $(".recipeForm").submit(function(event){
         event.preventDefault();
         var quant = getList(".ingredientQuant");
         var ingredient = getList(".ingredientItem");
+        var steps = getList(".step");
         console.log("Quantity: " + quant);
         console.log("Ingredients: " + ingredient);
+        console.log("Steps: " + steps);
+        var hiddenQuant = $("<input>")
+            .attr("type", "hidden")
+            .attr("name", "quantified_ingredients")
+            .attr("value", quant);
+        var hiddenIngredient = $("<input>")
+            .attr("type", "hidden")
+            .attr("name", "ingredients")
+            .attr("value", ingredient);
+        var hiddenSteps = $("<input>")
+            .attr("type", "hidden")
+            .attr("name", "steps")
+            .attr("value", steps);
+        $(".recipeForm").append(hiddenIngredient);
+        $(".recipeForm").append(hiddenQuant);
+        $(".recipeForm").append(hiddenSteps);
     })
 });
 
