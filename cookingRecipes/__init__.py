438664172e903c5edbf752870e9f34b333c86ca0
SophiaWeiler <sophia.weiler@outlook.com>
2023-12-11 12:37:12 +0100

    sw - correct db setting for final submission

--- cookingRecipes/__init__.py
+++ cookingRecipes/__init__.py
@@ -16,7 +16,7 @@ def create_app(test_config=None):
     # A secret for signing session cookies
     app.config["SECRET_KEY"] = "93220d9b340cf9a6c39bac99cce7daf220167498f91fa"
     
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///cookingRecipes.db"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqldb://24_webapp_007:6YsJBKIo@mysql.lab.it.uc3m.es/24_webapp_007a"
     # Register blueprints
     # (we import main from here to avoid circular imports in the next lab)
     from . import main
